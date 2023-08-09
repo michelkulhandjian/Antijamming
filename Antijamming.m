8e9b97cfc8442e000dae9e88b6fee9c8bc3ddd10
Michel Kulhandjian <34974925+michelkulhandjian@users.noreply.github.com>
2023-08-09 16:31:37 -0500

    added SIM_CHANNEL

--- Antijamming.m
+++ Antijamming.m
@@ -1,4 +1,4 @@
-function Antijamming (txPathDir, rxPathDir, dataPathDir, runType)
+function Antijamming (txPathDir, rxPathDir, dataPathDir, snr, runType)
 
 % Enable debug logs
 rP.debugLog  = true;
@@ -6,7 +6,7 @@ rP.debugLog  = true;
 pR = process; % initialize the process class
 
 if ~exist('runType','var')
-    runType   = "TX"; % Options: {"TX", "RX", "DATA_VISUALIZATION", "SIMULATE" }
+    runType   = "SIM_CHANNEL"; % Options: {"TX", "RX", "DATA_VISUALIZATION", "SIMULATE", "SIM_CHANNEL" }
 end
 
 switch runType
@@ -40,6 +40,7 @@ switch runType
         if ~exist('txPathDir','var')
             txPathDir = 'tx_signals4';
             %txPathDir = 'input_signals';
+            txPathDir = 'Tx';
         end
         if rP.debugLog
             fprintf( 'txPathDir: %s\n', txPathDir);
@@ -47,7 +48,7 @@ switch runType
         if ~exist('rxPathDir','var')
             %rxPathDir = 'rx_signals';
             %rxPathDir = 'output_signals';
-            rxPathDir = 'tx_signals4';
+            rxPathDir = 'Rx_GR_OTA';
         end
         if rP.debugLog
             fprintf( 'rxPathDir: %s\n', rxPathDir);
@@ -56,6 +57,42 @@ switch runType
         rP.MFSK = true; rP.QAM = true; rP.LDS = true; rP.LDSO=true;
         rP.isOFDM = true; rP.isSynch = true;
         pR.processRx(txPathDir, rxPathDir, rP);
+
+        time_s = (clock - start)*[0 0 24*60^2 60.^[2 1 0]]';
+        elapsed = fix(mod(time_s, [0 24*60^2, 60^2, 60])./[24*60^2, 60^2, 60, 1]);
+        if rP.debugLog
+            fprintf( 'Finished Processing Received waveform signals %d days, %d hours, %d min, %d sec \n', elapsed(1), elapsed(2), elapsed(3), elapsed(4));
+        end
+
+    case "SIM_CHANNEL"
+
+        if rP.debugLog
+            fprintf(" Processing Tx waveform Signals... \n");
+        end
+        start = clock;
+        if ~exist('txPathDir','var')
+            txPathDir = 'Tx';
+        end
+        if rP.debugLog
+            fprintf( 'txPathDir: %s\n', txPathDir);
+        end
+        if ~exist('rxPathDir','var')
+            rxPathDir = 'Rx2';
+        end
+        if rP.debugLog
+            fprintf( 'rxPathDir: %s\n', rxPathDir);
+        end
+
+        if ~exist('snr','var')
+            snr = 0;
+        end
+        if rP.debugLog
+            fprintf( 'snr : %d\n', snr);
+        end
+
+        rP.MFSK = true; rP.QAM = true; rP.LDS = true; rP.LDSO=true;
+        rP.isOFDM = true; rP.isSynch = true;
+        pR.processSimChannel(txPathDir, rxPathDir, snr, rP);
 
         time_s = (clock - start)*[0 0 24*60^2 60.^[2 1 0]]';
         elapsed = fix(mod(time_s, [0 24*60^2, 60^2, 60])./[24*60^2, 60^2, 60, 1]);
